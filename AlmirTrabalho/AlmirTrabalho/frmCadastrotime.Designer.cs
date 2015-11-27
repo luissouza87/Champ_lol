b4c702e9a29570e9bb9bd3e1e89512291d31de14
luissouza87 <luissouza87@hotmail.com>
2015-11-27 14:23:45 -0200

    tela cadastro de times
    layout pronto, arrumar bll

--- AlmirTrabalho/AlmirTrabalho/frmCadastrotime.Designer.cs
+++ AlmirTrabalho/AlmirTrabalho/frmCadastrotime.Designer.cs
@@ -35,15 +35,19 @@
             this.txtNome = new System.Windows.Forms.TextBox();
             this.txtPais = new System.Windows.Forms.TextBox();
             this.txtCriador = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            this.btnCadastrar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.btnSair = new System.Windows.Forms.Button();
             this.dgvCadasTimes = new System.Windows.Forms.DataGridView();
             this.lblQuantidade = new System.Windows.Forms.Label();
             this.lblMax = new System.Windows.Forms.Label();
             this.btUpdate = new System.Windows.Forms.Button();
             this.btDelete = new System.Windows.Forms.Button();
             this.txtId = new System.Windows.Forms.TextBox();
+            this.btnCad = new System.Windows.Forms.Button();
+            this.btnAtualizar = new System.Windows.Forms.Button();
+            this.btnDeletar = new System.Windows.Forms.Button();
+            this.lblInfo = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dgvCadasTimes)).BeginInit();
             this.SuspendLayout();
             // 
@@ -51,7 +55,7 @@
             // 
             this.lblid.AutoSize = true;
             this.lblid.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblid.Location = new System.Drawing.Point(48, 23);
+            this.lblid.Location = new System.Drawing.Point(45, 39);
             this.lblid.Name = "lblid";
             this.lblid.Size = new System.Drawing.Size(52, 27);
             this.lblid.TabIndex = 0;
@@ -61,7 +65,7 @@
             // 
             this.lblNome.AutoSize = true;
             this.lblNome.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblNome.Location = new System.Drawing.Point(21, 54);
+            this.lblNome.Location = new System.Drawing.Point(18, 70);
             this.lblNome.Name = "lblNome";
             this.lblNome.Size = new System.Drawing.Size(79, 27);
             this.lblNome.TabIndex = 1;
@@ -71,7 +75,7 @@
             // 
             this.lblCriador.AutoSize = true;
             this.lblCriador.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblCriador.Location = new System.Drawing.Point(5, 85);
+            this.lblCriador.Location = new System.Drawing.Point(2, 101);
             this.lblCriador.Name = "lblCriador";
             this.lblCriador.Size = new System.Drawing.Size(95, 27);
             this.lblCriador.TabIndex = 2;
@@ -81,7 +85,7 @@
             // 
             this.lblPais.AutoSize = true;
             this.lblPais.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblPais.Location = new System.Drawing.Point(37, 116);
+            this.lblPais.Location = new System.Drawing.Point(34, 132);
             this.lblPais.Name = "lblPais";
             this.lblPais.Size = new System.Drawing.Size(63, 27);
             this.lblPais.TabIndex = 3;
@@ -89,70 +93,72 @@
             // 
             // txtNome
             // 
-            this.txtNome.Location = new System.Drawing.Point(106, 59);
+            this.txtNome.Location = new System.Drawing.Point(103, 75);
             this.txtNome.Name = "txtNome";
             this.txtNome.Size = new System.Drawing.Size(282, 20);
             this.txtNome.TabIndex = 5;
             // 
             // txtPais
             // 
-            this.txtPais.Location = new System.Drawing.Point(106, 117);
+            this.txtPais.Location = new System.Drawing.Point(103, 133);
             this.txtPais.Name = "txtPais";
             this.txtPais.Size = new System.Drawing.Size(282, 20);
             this.txtPais.TabIndex = 6;
             // 
             // txtCriador
             // 
-            this.txtCriador.Location = new System.Drawing.Point(106, 88);
+            this.txtCriador.Location = new System.Drawing.Point(103, 104);
             this.txtCriador.Name = "txtCriador";
             this.txtCriador.Size = new System.Drawing.Size(282, 20);
             this.txtCriador.TabIndex = 7;
             // 
-            // button1
+            // btnCadastrar
             // 
-            this.button1.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Bold);
-            this.button1.Location = new System.Drawing.Point(107, 196);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 8;
-            this.button1.Text = "&Incluir";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.btnCadastrar.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Bold);
+            this.btnCadastrar.Location = new System.Drawing.Point(12, 226);
+            this.btnCadastrar.Name = "btnCadastrar";
+            this.btnCadastrar.Size = new System.Drawing.Size(94, 23);
+            this.btnCadastrar.TabIndex = 8;
+            this.btnCadastrar.Text = "Confirmar";
+            this.btnCadastrar.UseVisualStyleBackColor = true;
+            this.btnCadastrar.Click += new System.EventHandler(this.btnCadastrar_Click);
             // 
-            // button2
+            // btnCancelar
             // 
-            this.button2.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Bold);
-            this.button2.Location = new System.Drawing.Point(210, 196);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 9;
-            this.button2.Text = "&Cancelar";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btnCancelar.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Bold);
+            this.btnCancelar.Location = new System.Drawing.Point(318, 197);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(75, 52);
+            this.btnCancelar.TabIndex = 9;
+            this.btnCancelar.Text = "&Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
             // 
-            // button3
+            // btnSair
             // 
-            this.button3.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Bold);
-            this.button3.Location = new System.Drawing.Point(313, 196);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 10;
-            this.button3.Text = "&Sair";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.btnSair.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Bold);
+            this.btnSair.Location = new System.Drawing.Point(12, 358);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(381, 23);
+            this.btnSair.TabIndex = 10;
+            this.btnSair.Text = "&Sair";
+            this.btnSair.UseVisualStyleBackColor = true;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
             // 
             // dgvCadasTimes
             // 
             this.dgvCadasTimes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvCadasTimes.Location = new System.Drawing.Point(12, 225);
+            this.dgvCadasTimes.Location = new System.Drawing.Point(12, 255);
             this.dgvCadasTimes.Name = "dgvCadasTimes";
             this.dgvCadasTimes.Size = new System.Drawing.Size(381, 97);
             this.dgvCadasTimes.TabIndex = 11;
+            this.dgvCadasTimes.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.dgvCadasTimes_MouseDoubleClick);
             // 
             // lblQuantidade
             // 
             this.lblQuantidade.AutoSize = true;
             this.lblQuantidade.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblQuantidade.Location = new System.Drawing.Point(12, 143);
+            this.lblQuantidade.Location = new System.Drawing.Point(9, 159);
             this.lblQuantidade.Name = "lblQuantidade";
             this.lblQuantidade.Size = new System.Drawing.Size(192, 27);
             this.lblQuantidade.TabIndex = 12;
@@ -161,8 +167,9 @@
             // lblMax
             // 
             this.lblMax.AutoSize = true;
+            this.lblMax.Enabled = false;
             this.lblMax.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblMax.Location = new System.Drawing.Point(210, 143);
+            this.lblMax.Location = new System.Drawing.Point(207, 159);
             this.lblMax.Name = "lblMax";
             this.lblMax.Size = new System.Drawing.Size(24, 27);
             this.lblMax.TabIndex = 13;
@@ -171,46 +178,95 @@
             // btUpdate
             // 
             this.btUpdate.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Bold);
-            this.btUpdate.Location = new System.Drawing.Point(106, 167);
+            this.btUpdate.Location = new System.Drawing.Point(116, 226);
             this.btUpdate.Name = "btUpdate";
-            this.btUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btUpdate.Size = new System.Drawing.Size(94, 23);
             this.btUpdate.TabIndex = 14;
-            this.btUpdate.Text = "&Update";
+            this.btUpdate.Text = "Confirmar";
             this.btUpdate.UseVisualStyleBackColor = true;
             this.btUpdate.Click += new System.EventHandler(this.btUpdate_Click);
             // 
             // btDelete
             // 
             this.btDelete.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Bold);
-            this.btDelete.Location = new System.Drawing.Point(210, 167);
+            this.btDelete.Location = new System.Drawing.Point(216, 226);
             this.btDelete.Name = "btDelete";
-            this.btDelete.Size = new System.Drawing.Size(75, 23);
+            this.btDelete.Size = new System.Drawing.Size(94, 23);
             this.btDelete.TabIndex = 15;
-            this.btDelete.Text = "&Delete";
+            this.btDelete.Text = "Confirmar";
             this.btDelete.UseVisualStyleBackColor = true;
             this.btDelete.Click += new System.EventHandler(this.btDelete_Click);
             // 
             // txtId
             // 
-            this.txtId.Location = new System.Drawing.Point(106, 23);
+            this.txtId.Enabled = false;
+            this.txtId.Location = new System.Drawing.Point(103, 39);
             this.txtId.Name = "txtId";
             this.txtId.Size = new System.Drawing.Size(39, 20);
             this.txtId.TabIndex = 16;
+            // 
+            // btnCad
+            // 
+            this.btnCad.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Bold);
+            this.btnCad.Location = new System.Drawing.Point(12, 197);
+            this.btnCad.Name = "btnCad";
+            this.btnCad.Size = new System.Drawing.Size(94, 23);
+            this.btnCad.TabIndex = 17;
+            this.btnCad.Text = "&Cadastrar";
+            this.btnCad.UseVisualStyleBackColor = true;
+            this.btnCad.Click += new System.EventHandler(this.btnCad_Click);
+            // 
+            // btnAtualizar
+            // 
+            this.btnAtualizar.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Bold);
+            this.btnAtualizar.Location = new System.Drawing.Point(114, 197);
+            this.btnAtualizar.Name = "btnAtualizar";
+            this.btnAtualizar.Size = new System.Drawing.Size(94, 23);
+            this.btnAtualizar.TabIndex = 18;
+            this.btnAtualizar.Text = "&Atualizar";
+            this.btnAtualizar.UseVisualStyleBackColor = true;
+            this.btnAtualizar.Click += new System.EventHandler(this.btnAtualizar_Click);
+            // 
+            // btnDeletar
+            // 
+            this.btnDeletar.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Bold);
+            this.btnDeletar.Location = new System.Drawing.Point(216, 197);
+            this.btnDeletar.Name = "btnDeletar";
+            this.btnDeletar.Size = new System.Drawing.Size(94, 23);
+            this.btnDeletar.TabIndex = 19;
+            this.btnDeletar.Text = "&Deletar";
+            this.btnDeletar.UseVisualStyleBackColor = true;
+            this.btnDeletar.Click += new System.EventHandler(this.btnDeletar_Click);
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.AutoSize = true;
+            this.lblInfo.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInfo.ForeColor = System.Drawing.Color.Red;
+            this.lblInfo.Location = new System.Drawing.Point(98, 9);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(32, 27);
+            this.lblInfo.TabIndex = 20;
+            this.lblInfo.Text = "\\/";
             // 
             // frmCadastrotime
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(405, 334);
+            this.ClientSize = new System.Drawing.Size(405, 393);
+            this.Controls.Add(this.lblInfo);
+            this.Controls.Add(this.btnDeletar);
+            this.Controls.Add(this.btnAtualizar);
+            this.Controls.Add(this.btnCad);
             this.Controls.Add(this.txtId);
             this.Controls.Add(this.btDelete);
             this.Controls.Add(this.btUpdate);
             this.Controls.Add(this.lblMax);
             this.Controls.Add(this.lblQuantidade);
             this.Controls.Add(this.dgvCadasTimes);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnCadastrar);
             this.Controls.Add(this.txtCriador);
             this.Controls.Add(this.txtPais);
             this.Controls.Add(this.txtNome);
@@ -236,14 +292,18 @@
         private System.Windows.Forms.TextBox txtNome;
         private System.Windows.Forms.TextBox txtPais;
         private System.Windows.Forms.TextBox txtCriador;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button btnCadastrar;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Button btnSair;
         private System.Windows.Forms.DataGridView dgvCadasTimes;
         private System.Windows.Forms.Label lblQuantidade;
         private System.Windows.Forms.Label lblMax;
         private System.Windows.Forms.Button btUpdate;
         private System.Windows.Forms.Button btDelete;
         private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.Button btnCad;
+        private System.Windows.Forms.Button btnAtualizar;
+        private System.Windows.Forms.Button btnDeletar;
+        private System.Windows.Forms.Label lblInfo;
     }
 }
---
+++ AlmirTrabalho/AlmirTrabalho/frmCadastrotime.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AlmirTrabalho
+{
+    public partial class frmCadastrotime : Form
+    {
+        public frmCadastrotime()
+        {
+            InitializeComponent();
+        }
+
+        private void habilitaCampos(bool status)
+        {
+            txtNome.Enabled = status;
+            txtCriador.Enabled = status;
+            txtPais.Enabled = status;
+        }
+        private void btnControl(bool status)
+        {
+            btnCad.Enabled = status;
+            btnAtualizar.Enabled = status;
+            btnDeletar.Enabled = status;
+            btnSair.Enabled = status;
+        }
+
+        private void limpaCampos()
+        {
+            txtNome.Text = "";
+            txtCriador.Text = "";
+            txtPais.Text = "";
+            txtId.Text = "";
+        }
+
+        private void idHab(bool status, string info)
+        {
+            lblid.Visible = status;
+            txtId.Visible = status;
+            lblInfo.Visible = status;
+            lblInfo.Text = info;
+        }
+
+        /*private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Camadas.MODEL.time Times = new Camadas.MODEL.time();
+            Camadas.DAL.Time dalTimes = new Camadas.DAL.Time();
+            Times.nome = txtNome.Text;
+            Times.criador = txtCriador.Text;
+            Times.pais = txtPais.Text;
+            Times.qt_jogadores = Convert.ToInt32(lblMax.Text);
+
+            dalTimes.Insert(Times);
+            dgvCadasTimes.DataSource = dalTimes.Select();
+        }*/
+
+        private void frmCadastrotime_Load(object sender, EventArgs e)
+        {
+            limpaCampos();
+            Camadas.DAL.Time dalTime = new Camadas.DAL.Time();
+            dgvCadasTimes.DataSource = dalTime.Select();
+            habilitaCampos(false);
+            btnCancelar.Enabled = false;
+            btnCadastrar.Visible = false;
+            btUpdate.Visible = false;
+            btDelete.Visible = false;
+            idHab(false, @"\/");
+
+        }
+
+        private void btUpdate_Click(object sender, EventArgs e)
+        {
+            Camadas.MODEL.time Times = new Camadas.MODEL.time();
+            Camadas.DAL.Time daltime = new Camadas.DAL.Time();
+            Times.id = Convert.ToInt32(txtId.Text);
+            Times.nome = txtNome.Text;
+            Times.criador = txtCriador.Text;
+            Times.pais = txtPais.Text;
+            Times.qt_jogadores = Convert.ToInt32(lblMax.Text);
+
+
+            daltime.Update(Times);
+            dgvCadasTimes.DataSource = daltime.Select();
+            btnControl(true);
+            btUpdate.Visible = false;
+            limpaCampos();
+        }
+
+        private void btDelete_Click(object sender, EventArgs e)
+        {
+            Camadas.MODEL.time Times = new Camadas.MODEL.time();
+            Camadas.DAL.Time daltime = new Camadas.DAL.Time();
+            Times.id = Convert.ToInt32(txtId.Text);
+
+
+            daltime.Delete(Times);
+            dgvCadasTimes.DataSource = daltime.Select();
+            btnControl(true);
+            btDelete.Visible = false;
+            limpaCampos();
+        }
+
+        private void btnCadastrar_Click(object sender, EventArgs e)
+        {
+            Camadas.MODEL.time Times = new Camadas.MODEL.time();
+            Camadas.DAL.Time dalTimes = new Camadas.DAL.Time();
+            Times.nome = txtNome.Text;
+            Times.criador = txtCriador.Text;
+            Times.pais = txtPais.Text;
+            Times.qt_jogadores = Convert.ToInt32(lblMax.Text);
+
+            dalTimes.Insert(Times);
+            dgvCadasTimes.DataSource = dalTimes.Select();
+            btnControl(true);
+            btnCadastrar.Visible = false;
+            limpaCampos();
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            habilitaCampos(false);
+            btnCancelar.Enabled = false;
+            btnControl(true);
+            btnCadastrar.Visible = false;
+            btUpdate.Visible = false;
+            btDelete.Visible = false;
+            limpaCampos();
+            lblMax.Text = "5";
+            idHab(false, @"\/");
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnCad_Click(object sender, EventArgs e)
+        {
+            habilitaCampos(true);
+            btnCancelar.Enabled = true;
+            btnControl(false);
+            btnCadastrar.Visible = true;
+            limpaCampos();
+        }
+
+        private void btnAtualizar_Click(object sender, EventArgs e)
+        {
+            habilitaCampos(true);
+            btnCancelar.Enabled = true;
+            btnControl(false);
+            btUpdate.Visible = true;
+            btUpdate.Enabled = false;
+            limpaCampos();
+            idHab(true, @"Selecione abaixo p/ editar!");
+        }
+
+        private void btnDeletar_Click(object sender, EventArgs e)
+        {
+            btnCancelar.Enabled = true;
+            btnControl(false);
+            btDelete.Enabled = false;
+            btDelete.Visible = true;
+            limpaCampos();
+            idHab(true, @"Selecione abaixo p/ deletar!");
+        }
+
+        private void dgvCadasTimes_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            txtId.Text = dgvCadasTimes.SelectedRows[0].Cells[0].Value.ToString();
+            txtNome.Text = dgvCadasTimes.SelectedRows[0].Cells[1].Value.ToString();
+            txtCriador.Text = dgvCadasTimes.SelectedRows[0].Cells[2].Value.ToString();
+            txtPais.Text = dgvCadasTimes.SelectedRows[0].Cells[3].Value.ToString();
+            btUpdate.Enabled = true;
+            btDelete.Enabled = true;
+        }
+    }
+}
