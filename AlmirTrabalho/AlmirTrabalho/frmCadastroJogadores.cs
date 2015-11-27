d3199e5e378ca3378667e77236de2e6148480d87
luissouza87 <luissouza87@hotmail.com>
2015-11-27 04:55:26 -0200

    Tentativa de arrumar

--- AlmirTrabalho/AlmirTrabalho/frmCadastroJogadores.cs
+++ AlmirTrabalho/AlmirTrabalho/frmCadastroJogadores.cs
@@ -16,6 +16,20 @@ namespace AlmirTrabalho
         {
             InitializeComponent();
         }
+
+        private void recuperavalorTime()
+        {
+            List<Camadas.MODEL.time> listaTime = new List<Camadas.MODEL.time>();
+            Camadas.BLL.Time bllTime = new Camadas.BLL.Time();
+            int idTime = Convert.ToInt32(txbCodT.Text);
+            if (idTime > 0)
+            {
+                listaTime = bllTime.SelectById(idTime);
+                if (listaTime != null)
+                    txbCodT.Text = listaTime[0].id.ToString();
+                else txbCodT.Text = "0";
+            }
+        }
         private void habilitaCampos(bool status)
         {
             txbNome.Enabled = status;
@@ -32,18 +46,35 @@ namespace AlmirTrabalho
             habilitaCampos(false);
             lblTime.Visible = false;
             cbbTime.Visible = false;
+            lblCodT.Visible = false;
+            txbCodT.Visible = false;
+
+            Camadas.DAL.Jogador dalJogador = new Camadas.DAL.Jogador();
+            dgvJogadores.DataSource = dalJogador.Select();
+
+            List<Camadas.MODEL.time> ListaTime = new List<Camadas.MODEL.time>();
+            Camadas.BLL.Time bllTime = new Camadas.BLL.Time();
+            ListaTime = bllTime.Select();
+            cbbTime.DisplayMember = "nome";
+            cbbTime.ValueMember = "id";
+            cbbTime.DataSource = ListaTime;
+
         }
 
         private void rdbSemTime_CheckedChanged(object sender, EventArgs e)
         {
             lblTime.Visible = false;
             cbbTime.Visible = false;
+            lblCodT.Visible = false;
+            txbCodT.Visible = false;
         }
 
         private void rdbComTime_CheckedChanged(object sender, EventArgs e)
         {
             lblTime.Visible = true;
             cbbTime.Visible = true;
+            lblCodT.Visible = true;
+            txbCodT.Visible = true;
 
         }
 
@@ -69,6 +100,41 @@ namespace AlmirTrabalho
             rdbComTime.Checked = false;
             lblTime.Visible = false;
             cbbTime.Visible = false;
+            lblCodT.Visible = false;
+            txbCodT.Visible = false;
+        }
+
+        private void btnInserir_Click(object sender, EventArgs e)
+        {
+            Camadas.MODEL.Jogadores Jogador = new Camadas.MODEL.Jogadores();
+            Camadas.DAL.Jogador dalJogador = new Camadas.DAL.Jogador();
+            Jogador.id = Convert.ToInt32(lblId.Text);
+            Jogador.nome = txbNome.Text;
+            Jogador.idade = Convert.ToInt32(txbIdade.Text);
+            Jogador.nickname = txbNick.Text;
+            if (rdbSemTime.Checked == true)
+            {
+                Jogador.idTime = Convert.ToInt32(1);
+            }
+            else
+            {
+                Jogador.idTime = Convert.ToInt32(txbCodT.Text);
+            }
+
+            dalJogador.Insert(Jogador);
+            dgvJogadores.DataSource = dalJogador.Select();
+
+        }
+
+        private void cbbTime_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cbbTime_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+            txbCodT.Text = cbbTime.SelectedValue.ToString();
+            recuperavalorTime();
         }
     }
 }
